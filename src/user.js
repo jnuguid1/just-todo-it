294341984a0c82800f016baa705c2eb31bf7009e
Jared <jarednuguid@gmail.com>
2022-12-30 20:09:01 -0500

    Implement default user fields

--- src/user.js
+++ src/user.js
@@ -1,10 +1,32 @@
+import todoFactory from "./todo";
+import taskFactory from "./task";
+import projectFactory from "./project";
+
 const user = (() => {
   const projects = [];
+
+  const initializeUser = () => {
+    const projectName = 'Your Project';
+    const projectDesc = 'This is your project. This text blurb is your project description, put anything here.';
+    const firstProject = projectFactory(projectName, projectDesc);
+    const todoTitle = 'To do';
+    const todoDesc = 'This is a todo. Add checklist tasks, due dates, and priorities here.';
+    const dueDate = 'Tues, 10: 30 am';
+    const priority = 'Urgent';
+    const notes = 'Add notes and any additional thoughts here';
+    const firstTodo = todoFactory(todoTitle, todoDesc, dueDate, priority, notes);
+    const taskName = 'Make your first task';
+    const firstTask = taskFactory(taskName);
+    firstTodo.addTask(firstTask);
+    firstProject.addTodo(firstTodo);
+    addProject(firstProject);
+  };
+
+  initializeUser();
 
   const getProjects = () => {
     return projects;
   }
-
   const addProject = (project) => {
     projects.push(project);
   };
